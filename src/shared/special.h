d9de20f8cb9887a081313d039cff08c95eabbe48
Perevertun V.M. <vitalik.perevertun@gmail.com>
2015-09-03 17:36:08 +0300

    * journal: rename unit systemd-journald.socket to journald.socket;

--- src/shared/special.h
+++ src/shared/special.h
@@ -102,7 +102,7 @@
 /* Services systemd relies on */
 #define SPECIAL_DBUS_SERVICE "dbus.service"
 #define SPECIAL_DBUS_SOCKET "dbus.socket"
-#define SPECIAL_JOURNALD_SOCKET "systemd-journald.socket"
+#define SPECIAL_JOURNALD_SOCKET "journald.socket"
 #define SPECIAL_JOURNALD_SERVICE "systemd-journald.service"
 
 /* Magic init signals */
