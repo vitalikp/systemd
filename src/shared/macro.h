a3efbad4d767f3a6ef33b17d90c927645237c8e0
Perevertun V.M. <vitalik.perevertun@gmail.com>
2016-06-18 12:13:48 +0300

    * shared: add PTR_TO_SIZE macros;

--- src/shared/macro.h
+++ src/shared/macro.h
@@ -237,6 +237,8 @@ static inline unsigned long ALIGN_POWER2(unsigned long u) {
 #define PTR_TO_UINT64(p) ((uint64_t) ((uintptr_t) (p)))
 #define UINT64_TO_PTR(u) ((void *) ((uintptr_t) (u)))
 
+#define PTR_TO_SIZE(p) ((size_t) ((uintptr_t) (p)))
+
 #define memzero(x,l) (memset((x), 0, (l)))
 #define zero(x) (memzero(&(x), sizeof(x)))
 
