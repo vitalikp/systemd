774aaed642cd9c760dda446de63772973b0bc6e4
Perevertun V.M. <vitalik.perevertun@gmail.com>
2017-12-28 22:04:14 +0200

    * libsystemd: remove “bus_message_map_properties_changed” function;

--- src/libsystemd/sd-bus/bus-util.c
+++ src/libsystemd/sd-bus/bus-util.c
@@ -1040,43 +1040,6 @@ int bus_message_map_all_properties(sd_bus *bus,
         return sd_bus_message_exit_container(m);
 }
 
-int bus_message_map_properties_changed(sd_bus *bus,
-                                       sd_bus_message *m,
-                                       const struct bus_properties_map *map,
-                                       void *userdata) {
-        const char *member;
-        int r, invalidated, i;
-
-        assert(bus);
-        assert(m);
-        assert(map);
-
-        /* skip interface, but allow callers to do that themselves */
-        sd_bus_message_skip(m, "s");
-
-        r = bus_message_map_all_properties(bus, m, map, userdata);
-        if (r < 0)
-                return r;
-
-        r = sd_bus_message_enter_container(m, SD_BUS_TYPE_ARRAY, "s");
-        if (r < 0)
-                return r;
-
-        invalidated = 0;
-        while ((r = sd_bus_message_read_basic(m, SD_BUS_TYPE_STRING, &member)) > 0)
-                for (i = 0; map[i].member; i++)
-                        if (streq(map[i].member, member)) {
-                                ++invalidated;
-                                break;
-                        }
-
-        r = sd_bus_message_exit_container(m);
-        if (r < 0)
-                return r;
-
-        return invalidated;
-}
-
 int bus_map_all_properties(sd_bus *bus,
                            const char *destination,
                            const char *path,
